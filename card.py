a772d2601354413706269e307ca67511cd6cba93
gyorgy-s <140075614+gyorgy-s@users.noreply.github.com>
2023-10-08 17:56:53 +0200

    Initial commit

---
+++ card.py
@@ -0,0 +1,138 @@
+"""
+Card module for the flash card app.
+
+The sides of the cards are read from an external CSV, where each line consists
+of a pair of strings. Each line represents a card, with the first value being
+on the "front", the second on the "back" side of the card.
+
+The first line of the file represents the title for the values. In the example
+these being the French and English word pairs.
+
+The app reads the source CSV as to be able to handle the manipulation of the
+source file, as long as it adhedes the above mentioned rules.
+
+The user can flip the card by clicking on it to check their answer.
+If the user clicks on the "correct" button, the word is then removed from the
+pool, as it is already known. If the user clicks on the "wrong" button, the
+card will stay in the pool. In both cases, a new card will be picked from the
+pool.
+
+The user is able to switch the "front" and "back" of the cards by clicking the
+"switch" button. (The default order is given by the occurrence in the source CSV.
+This can be changed in the "card.py", by swapping the values of the
+"FRONT" and "BACK" globals (note that the only viable values are 0 and 1, and
+the two must be different, else the program will run on an error)
+
+THERE IS NO ERROR HANDLING CURRENTLY, THE FILES MUST BE PRESENT IN THE
+CORRECT FOLDERS FOR THE PROGRAM TO RUN PROPERLY.
+"""
+
+
+import os
+import random
+import pandas as pd
+
+
+# Global variables, colors and source file name
+BACKGROUND_COLOR = "#B1DDC6"
+FOREGROUND_COLOR = "#FFFFFF"
+
+DATA_SOURCE = "source.csv"
+
+# Front - back direction, if the default order should be switched, swap the
+# values of these. The default values are:
+# FRONT = 0
+# BACK = 1
+# Representing the order in which the word pairs are present in the CSV.
+FRONT = 0
+BACK = 1
+
+class Card:
+    """Card class for the flash cards. Implements the main functions and features of the flsh card."""
+    def __init__(self, titletext, wordtext, cardimg, cardfrontimg, cardbackimg, frontback):
+        # Initializing the dictionary. First reading the first row of the CSV,
+        # getting the header for the values. Then reaping the rest of the CSV,
+        # and compiling it into a dict.
+        df_header = pd.read_csv(
+            os.path.join("", "data", DATA_SOURCE),
+            index_col=None,
+            header=None,
+            nrows=1
+        )
+
+        self.title_front = df_header.iat[0,FRONT]
+        self.title_back = df_header.iat[0,BACK]
+
+        self.dictionary = pd.read_csv(
+            os.path.join("", "data", DATA_SOURCE),
+            index_col=0,
+            header=None,
+            skiprows=1
+        ).to_dict(orient="dict")[1]
+
+        self.current_word = self.get_random_word()
+        self.side = -1
+
+        # Attributes solely to handle the external widgets from TKinter.
+        self.title_text = titletext
+        self.word_text = wordtext
+        self.card_img = cardimg
+        self.card_front_img = cardfrontimg
+        self.card_back_img = cardbackimg
+        self.front_back = frontback
+
+    def get_random_word(self):
+        """Gets a random key-value pair from the dicitionary."""
+        if self.dictionary :
+            word = random.choice(list(self.dictionary.items()))
+        else:
+            word = ("THE END","THE END")
+        self.current_word = word
+        return word
+
+    def pop_word(self):
+        """Removes the current word from the dicitionary."""
+        if len(self.dictionary) > 0:
+            self.dictionary.pop(self.current_word[0])
+
+    def set_word(self, canv):
+        """Updates the Tk widgets according the current words."""
+        if self.side < 0:
+            canv.itemconfig(self.title_text, text=self.title_front, fill=BACKGROUND_COLOR)
+            canv.itemconfig(self.word_text, text=self.current_word[FRONT], fill=BACKGROUND_COLOR)
+            canv.itemconfig(self.card_img, image=self.card_front_img)
+        elif self.side > 0:
+            canv.itemconfig(self.title_text, text=self.title_back, fill=FOREGROUND_COLOR)
+            canv.itemconfig(self.word_text, text=self.current_word[BACK], fill=FOREGROUND_COLOR)
+            canv.itemconfig(self.card_img, image=self.card_back_img)
+
+    def flip(self, event, canv):
+        """Flips the card's front and back, then updates the widgets."""
+        self.side *= -1
+        self.set_word(canv)
+
+
+    def correct(self, canv):
+        """If the user was correct, removes the current word from the pool,
+        then gets a new one and updates the widgets."""
+        self.pop_word()
+        self.get_random_word()
+        self.side = -1
+        self.set_word(canv)
+
+
+    def wrong(self, canv):
+        """Id the user was wrong, gets a new card from the pool and updates the widgets."""
+        self.get_random_word()
+        self.side = -1
+        self.set_word(canv)
+
+    def change_order(self, canv):
+        """Swaps the cards' front and back."""
+        global FRONT
+        global BACK
+
+        FRONT, BACK = BACK, FRONT
+        self.title_front, self.title_back = self.title_back, self.title_front
+        self.front_back.config(text=f"{self.title_front} <-> {self.title_back}")
+        self.set_word(canv)
---
+++ main.py
@@ -0,0 +1,130 @@
+"""
+Main module for the falsh card app.
+
+It is an app with a GUI to implement a flash card app. The sides of the cards
+are read from an external CSV, where each line consists of a pair of strings.
+Each line represents a card, with the first value being on the "front", the
+second on the "back" side of the card.
+
+The first line of the file represents the title for the values. In the example
+these being the French and English word pairs.
+
+The app reads the source CSV as to be able to handle the manipulation of the
+source file, as long as it adhedes the above mentioned rules.
+
+The user can flip the card by clicking on it to check their answer.
+If the user clicks on the "correct" button, the word is then removed from the
+pool, as it is already known. If the user clicks on the "wrong" button, the
+card will stay in the pool. In both cases, a new card will be picked from the
+pool.
+
+The user is able to switch the "front" and "back" of the cards by clicking the
+"switch" button. (The default order is given by the occurrence in the source CSV.
+This can be changed in the "card.py" as well, by swapping the values of the
+"FRONT" and "BACK" globals (note that the only viable values are 0 and 1, and
+the two must be different, else the program will run on an error)
+
+THERE IS NO ERROR HANDLING CURRENTLY, THE FILES MUST BE PRESENT IN THE
+CORRECT FOLDERS FOR THE PROGRAM TO RUN PROPERLY.
+"""
+
+
+import os
+import tkinter as tk
+
+import card
+
+# Global variables, colors and fonts
+BACKGROUND_COLOR = "#B1DDC6"
+FOREGROUND_COLOR = "#FFFFFF"
+
+FONT_NAME = ""
+FONT_SIZE = 40
+
+FONT_TITLE = (FONT_NAME, FONT_SIZE, "normal")
+FONT_WORD = (FONT_NAME, FONT_SIZE * 2, "bold")
+FONT_SWITCH = (FONT_NAME, FONT_SIZE // 3, "bold")
+
+
+if __name__ == "__main__":
+
+    # Creating the main window for the app.
+    window = tk.Tk()
+    window.title("Flash Cards")
+    window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+
+    # Loading the images.
+    card_front_img = tk.PhotoImage(file=os.path.join("", "images", "card_front.png"))
+    card_back_img = tk.PhotoImage(file=os.path.join("", "images", "card_back.png"))
+    correct_img = tk.PhotoImage(file=os.path.join("", "images", "correct.png"))
+    wrong_img = tk.PhotoImage(file=os.path.join("", "images", "wrong.png"))
+    switch_img = tk.PhotoImage(file=os.path.join("", "images", "switch.png"))
+
+
+    # Creating the canvas, to place the "card" in the window.
+    canvas = tk.Canvas(
+        window, height=526, width=800, bg=BACKGROUND_COLOR, highlightthickness=0
+    )
+    card_img = canvas.create_image(400, 263, image=card_front_img)
+    canvas.bind("<Button-1>",func=lambda x: flash_card.flip(x, canvas))
+    title_text = canvas.create_text(400, 150, font=FONT_TITLE)
+    word_text = canvas.create_text(400, 263, font=FONT_WORD)
+
+    # The front and back direction label.
+    front_back = tk.Label(
+        text=f"",
+        bg=BACKGROUND_COLOR,
+        foreground=FOREGROUND_COLOR,
+        border=0,
+        font=FONT_SWITCH
+    )
+
+    # Creatin the card class, passing the canvas elements as argument, as these
+    # will be modified within the class methods.
+    flash_card = card.Card(
+        titletext=title_text,
+        wordtext=word_text,
+        cardimg=card_img,
+        cardfrontimg=card_front_img,
+        cardbackimg=card_back_img,
+        frontback=front_back
+    )
+
+    # Buttons for the correct, worng guess and the switch direction.
+    wrong_button = tk.Button(
+        image=wrong_img,
+        highlightthickness=0,
+        command=lambda: flash_card.wrong(canvas),
+        border=0
+    )
+
+    correct_button = tk.Button(
+        image=correct_img,
+        highlightthickness=0,
+        command=lambda: flash_card.correct(canvas),
+        border=0,
+    )
+
+    switch_button = tk.Button(
+        image=switch_img,
+        highlightthickness=0,
+        command=lambda: flash_card.change_order(canvas),
+        border=0
+    )
+
+
+    # Placement of the elements to the window.
+    canvas.grid(column=0, row=0, columnspan=2)
+    wrong_button.grid(column=0, row=1)
+    correct_button.grid(column=1, row=1)
+    switch_button.grid(column=0, row=3, columnspan=2)
+    front_back.grid(column=0, row=4, columnspan=2)
+
+
+    # Initial setup for the words on the card and the direction indicator.
+    flash_card.set_word(canvas)
+    front_back.config(text=f"{flash_card.title_front} <-> {flash_card.title_back}")
+
+
+    window.mainloop()
